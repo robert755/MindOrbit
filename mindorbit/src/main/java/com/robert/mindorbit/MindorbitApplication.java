81936bb569c11c9a5e261b3f7a190d4c49f1ea8e
robert755 <106335134+robert755@users.noreply.github.com>
2026-03-06 11:31:52 +0200

    backend
    initial push after creating backend and dependecies

---
+++ mindorbit/src/main/java/com/robert/mindorbit/MindorbitApplication.java
@@ -0,0 +1,13 @@
+package com.robert.mindorbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindorbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindorbitApplication.class, args);
+	}
+
+}
---
+++ mindorbit/src/test/java/com/robert/mindorbit/MindorbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robert.mindorbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindorbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
